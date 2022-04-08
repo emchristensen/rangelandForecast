527bfc8d9c6ded10fbbcd5be8088c51f6b83e1e3
Sarah Chisholm <89938738+sarah-chisholm@users.noreply.github.com>
2022-04-08 16:12:50 +0000

    Mask state class rasters by elevation range and soil raster

--- Scripts/1-mask-state-class-rasters.R
+++ Scripts/1-mask-state-class-rasters.R
@@ -1,4 +1,7 @@
-# Mask state class rasters to elevation between 1000-2000m
+# a248
+# Sarah Chisholm (ApexRMS)
+#
+# Mask state class rasters to soil type raster and to an elevation range of 1000-2000m 
 
 ## Workspace ----
 
@@ -13,6 +16,9 @@ spatialDataDir <- file.path("Data", "Spatial")
 # Elevation raster
 elevationRaster <- rast(file.path(spatialDataDir, "MLRA42_DEM1arcsec.tif"))
 
+# Soil types
+soilTypeRaster <- rast(file.path(spatialDataDir, "NM_ens_dsm_prediction.tif"))[[8]]
+
 # State class raster file paths
 stateClassFileList <- list.files(file.path(spatialDataDir, "RAP_processed"),
                                  pattern = "categories.tif",
@@ -21,20 +27,36 @@ stateClassFileList <- list.files(file.path(spatialDataDir, "RAP_processed"),
 # Parameters
 targetCrs <- "epsg:3083"
 
-## Mask by elevation ----
+## Create mask raster ----
 # Load a template raster on the correct grid
 templateRaster <- rast(stateClassFileList[1]) %>% 
   project(y = targetCrs)
 
-# Adjust the resolution and extent of the elevation raster
-elevationRaster <- elevationRaster %>% 
-  resample(y = templateRaster)
+# Adjust the crs, resolution and extent of the elevation raster
+# zzz: review that these are the correct steps to get onto common grid
+elevationRaster <- elevationRaster %>%
+  project(y = templateRaster) 
 
 # Create elevation mask raster
-maskRaster <- elevationRaster
-maskRaster[maskRaster > 2000] <- NA
-maskRaster[maskRaster < 1000] <- NA
+elevationMaskRaster <- elevationRaster
+elevationMaskRaster[elevationMaskRaster > 2000] <- NA
+elevationMaskRaster[elevationMaskRaster < 1000] <- NA
 
+# Adjust the crs, resolution and extent of the elevation raster
+# zzz: review that these are the correct steps to get onto common grid
+soilTypeRaster <- soilTypeRaster %>% 
+  project(y = templateRaster)
+
+# Combine soil classes 1 (sandy) and 2 (deep sand)
+# Reassign to new soil class 8 (sandy and deep sand)
+soilTypeMaskRaster <- soilTypeRaster
+soilTypeMaskRaster[soilTypeMaskRaster == 1] <- 8
+soilTypeMaskRaster[soilTypeMaskRaster == 2] <- 8
+
+# Combine elevation and soil type raster to make one mask raster
+maskRaster <- elevationMaskRaster + soilTypeMaskRaster
+
+## Apply elevation and soil mask ----
 # Loop over state class files and apply elevation mask
 for(stateClassFile in stateClassFileList){
   
