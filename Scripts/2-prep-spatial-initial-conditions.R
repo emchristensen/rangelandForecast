5e42ad321d2cc6ae9d5e6dc28e1a71018d8c3cb6
Sarah Chisholm <89938738+sarah-chisholm@users.noreply.github.com>
2022-09-19 16:01:17 +0000

    Create initial condition rasters for baseline and forecast scenarios

---
+++ Scripts/2-prep-spatial-initial-conditions.R
@@ -0,0 +1,92 @@
+## rangelandForecast
+## Sarah Chisholm, ApexRMS
+##
+## Create spatial initial conditions
+
+## Workspace ----
+
+# Set environment variable TZ when running on AWS EC2 instance
+Sys.setenv(TZ='UTC')
+
+# Load libraries
+library(tidyverse)
+library(terra)
+
+# Define directories
+spatialDataDir <- file.path("Data", "Spatial")
+spatialModelInputsDir <- file.path("Model-Inputs", "Spatial")
+
+# Parameters
+# Test extent
+targetExtent <- ext(809000, 875000, 7600000, 7666000)
+
+# Load spatial data
+# Studay area mask
+studyAreaMaskRaster <- rast(file.path(spatialDataDir, "study-area-mask.tif"))
+
+# Soil type raster
+soilTypeRaster <- rast(file.path(spatialDataDir, "soil-type.tif"))
+
+# State class map (1985)
+stateClass1985Raster <- rast(file.path(spatialDataDir, "RCMAP", "Processed", "rcmap_states_1985.tif"))
+
+# State class map (2020)
+stateClass2020Raster <- rast(file.path(spatialDataDir, "RCMAP", "Processed", "rcmap_states_2020.tif"))
+
+## Create Spatial Initial Conditions ----
+
+## Crop, mask, and reclassify soil type raster ----
+# Combine soil classes 1 (sandy) and 2 (deep sand)
+# Reassign to new soil class 8 (sandy and deep sand)
+soilTypeReclassMatrix <- matrix(data = c(1,2,3,4,5,6,7,8,8,3,4,5,6,7), nrow = 7, ncol = 2)
+
+# Soil type - Full Extent
+soilTypeRaster <- soilTypeRaster %>% 
+  project(y = studyAreaMaskRaster, method = "near") %>% 
+  mask(mask = studyAreaMaskRaster) %>% 
+  classify(rcl = soilTypeReclassMatrix) %>% 
+  writeRaster(filename = file.path(spatialModelInputsDir, "soil-type.tif"),
+              datatype = "INT2S",
+              NAflag = -9999,
+              overwrite = TRUE)
+
+# Soil type - Test Extent
+test <- soilTypeRaster %>% 
+  project(y = studyAreaMaskRaster, method = "near") %>% 
+  mask(mask = studyAreaMaskRaster) %>% 
+  crop(y = targetExtent)%>% 
+  classify(rcl = soilTypeReclassMatrix)%>% 
+  writeRaster(filename = file.path(spatialModelInputsDir, "soil-type-test-extent.tif"),
+              datatype = "INT2S",
+              NAflag = -9999,
+              overwrite = TRUE)
+
+## Crop and mask the state class raster ----
+
+# Initial state class - 1985
+stateClass1985Raster <- stateClass1985Raster %>% 
+  project(y = studyAreaMaskRaster, method = "near") %>% 
+  mask(mask = studyAreaMaskRaster) %>% 
+  writeRaster(filename = file.path(spatialModelInputsDir, "state-class-1985.tif"),
+              datatype = "INT2S",
+              NAflag = -9999,
+              overwrite = TRUE)
+
+# Initial state class - 2020
+stateClass2020Raster <- stateClass2020Raster %>% 
+  project(y = studyAreaMaskRaster, method = "near") %>% 
+  mask(mask = studyAreaMaskRaster) %>% 
+  writeRaster(filename = file.path(spatialModelInputsDir, "state-class-2020.tif"),
+              datatype = "INT2S",
+              NAflag = -9999,
+              overwrite = TRUE)
+
+# Initial state class - Test Extent
+test <- stateClass1985Raster %>% 
+  project(y = studyAreaMaskRaster, method = "near") %>% 
+  mask(mask = studyAreaMaskRaster) %>% 
+  crop(y = targetExtent)%>%  
+  writeRaster(filename = file.path(spatialModelInputsDir, "state-class-test-extent-1985.tif"),
+              datatype = "INT2S",
+              NAflag = -9999,
+              overwrite = TRUE)
